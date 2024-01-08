259ec8e61539184c5799583d62331abad946c09a
Văn Hiếu <hieu.dovan@thegioididong.com>
2024-01-08 11:23:28 +0700

    update

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const axios = require('axios');
 const cors = require('cors');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 app.use(cors());
 require('dotenv').config();
 const currentTime = new Date();
