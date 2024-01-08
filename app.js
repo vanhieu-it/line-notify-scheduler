1a219f41f2ea8f73d9768f81d4f55840c5469763
Văn Hiếu <hieu.dovan@thegioididong.com>
2024-01-08 10:36:46 +0700

    update

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ const axios = require('axios');
 const schedule = require('node-schedule');
 require('dotenv').config(); // Đọc biến môi trường từ file .env
 
+
+
 const accessToken = process.env.ACCESS_TOKEN; // Sử dụng biến môi trường
 const notifyEndpoint = 'https://notify-api.line.me/api/notify';
-const message = 'Lịch nhắc: Bây giờ là 10h30p';
+const message = 'Lịch nhắc: Test';
 
-const job = schedule.scheduleJob('30 10 * * *', () => {
+const job = schedule.scheduleJob('40 10 * * *', () => {
     axios.post(
         notifyEndpoint,
         `message=${message}`,
